36d3fcaf798f40b40db73fce4b136298cae48db4
Eugen <eugen.krotov@cloud-surfers.de>
2024-06-26 17:08:55 +0200

    converting to ts

--- test.ts
+++ test.ts
@@ -7,7 +7,7 @@ let rl = readline.createInterface(
     process.stdin, process.stdout);
 
 let filePath = "foo.txt"
-rl.question('Gib die Datei an: ', function(path) {
+rl.question('Gib die Datei an: ', function(path:string) {
     console.log(`provide an absolut path 2 file: ${path}`);
     filePath = path;
     let buffer = fs.readFileSync(filePath,'utf8');
