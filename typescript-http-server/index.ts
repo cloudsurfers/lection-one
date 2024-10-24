819dabf0ede1fdde42ba943d688d2504afefd81d
Eugen <eugen.krotov@cloud-surfers.de>
2024-10-24 14:44:35 +0200

    working http server with cars

--- typescript-http-server/index.ts
+++ typescript-http-server/index.ts
@@ -1,4 +1,51 @@
-console.log("hallo ts mama")
-for(var i=0;i<10;i++){
-	console.log(i)
+import * as http from "http";
+import * as fs from "fs";
+import * as path from "path";
+
+const  server = http.createServer((req, res) =>{
+	if (req.method == "GET" && req.url === "/autos"){
+		handleGetCars(req, res)
+	}
+	if (req.method == "POST" && req.url == "/auto"){
+		handleAddCar(req, res)
+	}
+	
+	//if (req.method)
+	//res.write('hallo ts welt '+req.url)
+	//res.end()
+});
+
+server.listen(4200, ()=>{
+	console.log("server started")
+});
+
+function handleAddCar(req:http.IncomingMessage, res:http.ServerResponse){
+	let data = ""
+	req.on("data", (x)=>{
+		console.log("x="+x)
+		data = x
+		res.end()
+	})
 }
+
+function handleGetCars(req:http.IncomingMessage, res:http.ServerResponse){
+	res.setHeader("Content-Type", "application/json")
+	loadCarList(res)
+}
+
+function loadCarList(res:http.ServerResponse){
+	let p = path.join(__dirname, "test.json")
+	fs.readFile(p,null, (er, data)=>{
+		if (er!=null){
+			console.log("failed to read from file system: "+p+" with error "+er)
+			res.setHeader("Status", 500)
+			res.end(er)
+			return
+		}
+		console.log("data from filesystem : "+data)
+		res.end(data)
+	})
+	
+}
+
+//server.close()
